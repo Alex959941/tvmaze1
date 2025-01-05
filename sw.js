ccded9288b00f14e0b5ccb6fd3a8d36f67cbc5bc
Alex959941 <1469436@sd61learn.ca>
2025-01-04 16:49:45 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,64 @@
-const version = 'v123';  // change this everytime you update the service worker
-                          // to force the browser to also update it.
+// Define cache names
+const CACHE_NAME = 'my-app-cache-v1';
+const DYNAMIC_CACHE_NAME = 'my-app-dynamic-cache-v1';
 
-self.addEventListener('install', function(event) {
+// Files to cache during install
+const STATIC_ASSETS = [
+    'index.html',
+    'style.css',
+    'apiexample.js',
+    'icons/icon-72x72.png',
+    'icons/icon-512x512.png',
+    'icons/icon-96x96.png',
+    'icons/icon-128x128.png',
+    'icons/icon-144x144.png',
+    'icons/icon-152x152.png',
+    'icons/icon-192x192.png',
+    'icons/icon-384x384.png'
+  // Add other static files you want to cache
+];
+
+// Install event: Cache static assets
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'style.css',
-        'apiexample.js',
-        'icons/icon-72x72.png',
-        'icons/icon-512x512.png',
-        'icons/icon-96x96.png',
-        'icons/icon-128x128.png',
-        'icons/icon-144x144.png',
-        'icons/icon-152x152.png',
-        'icons/icon-192x192.png',
-        'icons/icon-384x384.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(STATIC_ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+// Activate event: Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME && cache !== DYNAMIC_CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
     })
+  );
+});
+
+// Fetch event: Network-first strategy
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        // If network fetch is successful, cache the response
+        return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        // If network fetch fails, fallback to cache
+        console.log("The app needs to be online to perform this action.")
+        return caches.match(event.request);
+      })
   );
 });
